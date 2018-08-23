1e8500809da42ed1ad9712e29ebe4862cab08632
wxiaowen <475796288@qq.com>
2018-08-24 00:06:07 +0800

    歌曲换成本地

--- js/weatherCtrl.js
+++ js/weatherCtrl.js
@@ -8,12 +8,7 @@ function sing() {
     var audio = document.getElementsByTagName('audio')[0];
     var play = document.getElementById("ic_play_img");
     var coverAlum = document.getElementById("cover_alum");
-    
-    if (audio.paused) {
-        audio.play();
-          load("./song/C400001J5QJL1pRQYB.mp3");
-           visulaLizer();
-        //适配各种浏览器
+      //适配各种浏览器
         coverAlum.style.animationName = "go";
         coverAlum.style.animationPlayState = "running";
         coverAlum.style.webkitAnimationName = "go";
@@ -24,19 +19,33 @@ function sing() {
         coverAlum.style.oAnimationPlayState = "running";
         coverAlum.style.oAnimationPlayState = "running";
         play.style.display = "none";
-    } else {
-        audio.pause();
-        //适配各种浏览器
-        coverAlum.style.animationName = "go";
-        coverAlum.style.animationPlayState = "paused";
-        coverAlum.style.webkitAnimationName = "go";
-        coverAlum.style.webkitAnimationPlayState = "paused";
-        coverAlum.style.mozAnimationName = "go";
-        coverAlum.style.mozAnimationPlayState = "paused";
-        coverAlum.style.oAnimationName = "go";
-        coverAlum.style.oAnimationPlayState = "paused";
-        play.style.display = "block";
-    }
+        load("./song/C400001J5QJL1pRQYB.mp3");
+    // if (audio.paused) {
+    //     audio.play();
+    //     //适配各种浏览器
+    //     coverAlum.style.animationName = "go";
+    //     coverAlum.style.animationPlayState = "running";
+    //     coverAlum.style.webkitAnimationName = "go";
+    //     coverAlum.style.webkitAnimationPlayState = "running";
+    //     coverAlum.style.mozAnimationName = "go";
+    //     coverAlum.style.mozAnimationPlayState = "running";
+    //     coverAlum.style.oAnimationName = "go";
+    //     coverAlum.style.oAnimationPlayState = "running";
+    //     coverAlum.style.oAnimationPlayState = "running";
+    //     play.style.display = "none";
+    // } else {
+    //     audio.pause();
+    //     //适配各种浏览器
+    //     coverAlum.style.animationName = "go";
+    //     coverAlum.style.animationPlayState = "paused";
+    //     coverAlum.style.webkitAnimationName = "go";
+    //     coverAlum.style.webkitAnimationPlayState = "paused";
+    //     coverAlum.style.mozAnimationName = "go";
+    //     coverAlum.style.mozAnimationPlayState = "paused";
+    //     coverAlum.style.oAnimationName = "go";
+    //     coverAlum.style.oAnimationPlayState = "paused";
+    //     play.style.display = "block";
+    // }
 }
 
 
@@ -122,8 +131,7 @@ analyser.getByteFrequencyData(arr);
     draw(arr);
 requestAnimationFrame(v);
     }
-    requestAnimationFrame(v);
-}
+    requestAnimationFrame(v);}
 function changeVolume(per){
     gainNode.gain.value=per*per;
 }
